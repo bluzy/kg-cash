b203388cd3808144bb6a6736a84ec6ae320a67fb
bluzy <bsoul87@gmail.com>
2020-04-11 16:15:28 +0900

    Updates

---
+++ precache-manifest.3965d5273194f33e8c2cbbeefc26672c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2033eaec96fae94c1eded3db7458cce",
+    "url": "/kg-cash/index.html"
+  },
+  {
+    "revision": "2292409c96d726bdaa16",
+    "url": "/kg-cash/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "66716b0676492bb95669",
+    "url": "/kg-cash/static/js/2.03605b03.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/kg-cash/static/js/2.03605b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2292409c96d726bdaa16",
+    "url": "/kg-cash/static/js/main.31c50510.chunk.js"
+  },
+  {
+    "revision": "2b4c73a61027f5325c17",
+    "url": "/kg-cash/static/js/runtime-main.b4ea08b7.js"
+  }
+]);
